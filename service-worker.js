1099a6d16010fcc808099cd92a534926218621c7
1rv1n <you@example.com>
2020-09-10 16:45:26 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.b3d3ef86bfad5147e26d7dca6dac079e.js"
+  "/portfolio/precache-manifest.6154f80494d81b581b82710fc47dcf8c.js"
 );
 
 self.addEventListener('message', (event) => {
