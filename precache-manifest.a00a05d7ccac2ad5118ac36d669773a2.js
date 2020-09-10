8afbd47beb2fc03923986ac4ca499fc40eec899b
1rv1n <you@example.com>
2020-09-11 01:14:22 +0200

    Updates

---
+++ precache-manifest.a00a05d7ccac2ad5118ac36d669773a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea723a08dce2c02148a6c95a4a7a5d7d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bb9c85e1c8cff8949396",
+    "url": "/portfolio/static/css/main.2c8ccf55.chunk.css"
+  },
+  {
+    "revision": "5103ad1484d214fb4d97",
+    "url": "/portfolio/static/js/2.a9fc07cd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.a9fc07cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb9c85e1c8cff8949396",
+    "url": "/portfolio/static/js/main.2962a39d.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "a082fd3df68a0b54e0d4d794bc38d268",
+    "url": "/portfolio/static/media/Blender-Pro-Bold.a082fd3d.ttf"
+  },
+  {
+    "revision": "335787aa6f57d71cabe8eb5dc89c6d6b",
+    "url": "/portfolio/static/media/Blender-Pro-Book.335787aa.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6154f80494d81b581b82710fc47dcf8c.js"
+  "/portfolio/precache-manifest.a00a05d7ccac2ad5118ac36d669773a2.js"
 );
 
 self.addEventListener('message', (event) => {
